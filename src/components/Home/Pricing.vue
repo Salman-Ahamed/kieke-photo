984d86d0a18fdb9957f35ee8200f35a41cd0cd02
Salman-Ahamed <me.shahriyarhosen@gmail.com>
2025-04-09 21:53:12 +0600

    Add dynamic progress bar and range input to Pricing component

--- src/components/Home/Pricing.vue
+++ src/components/Home/Pricing.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+import { computed, ref } from "vue";
 import TickCirle from "../../assets/icons/tick-circle.svg";
+
+const amount = ref(244); // default value
+const max = 1000;
+
+const progressPercent = computed(() => {
+  return (amount.value / max) * 100;
+});
 </script>
 
 <template>
@@ -21,7 +29,7 @@ import TickCirle from "../../assets/icons/tick-circle.svg";
       </div>
       <!-- -------- body ----------- -->
       <div class="mt-5">
-        <div class="h-[20px] bg-gray-200 rounded-sm relative">
+        <!-- <div class="h-[20px] bg-gray-200 rounded-sm relative">
           <div
             class="h-[20px] absolute top-0 left-0 bg-[#2D3B3B] min-w-[200px] max-w-[300px] w-[100%]"
           >
@@ -45,8 +53,48 @@ import TickCirle from "../../assets/icons/tick-circle.svg";
               $1,000
             </h1>
           </div>
-        </div>
+        </div> -->
         <!-- ....... -->
+        <div class="p-6">
+          <!-- Progress Bar Container -->
+          <div class="h-[20px] bg-gray-200 rounded-sm relative">
+            <!-- Dynamic Progress Bar -->
+            <div
+              class="h-[20px] absolute top-0 left-0 bg-[#2D3B3B] max-w-[100%] progress-bar"
+              :style="{ width: progressPercent + '%' }"
+            >
+              <!-- Dynamic Price Label -->
+              <div
+                class="w-[100px] flex items-center justify-center text-[16px] md:text-[23px] leading-[150%] text-center text-[#FFFFFF] font-[500] h-[30px] md:h-[40px] bg-[#2D3B3B] absolute -top-[50%] -right-[10px]"
+              >
+                ${{ amount }}
+              </div>
+            </div>
+            <div class="w-[100%] absolute opacity-0">
+              <input
+                type="range"
+                min="0"
+                max="1000"
+                v-model="amount"
+                class="w-[100%]"
+              />
+            </div>
+          </div>
+
+          <!-- Range Display -->
+          <div class="mt-3">
+            <div class="flex justify-between">
+              <h1 class="text-[16px] md:text-[23px] text-[#2D3B3B] font-[500]">
+                ${{ amount }}
+              </h1>
+              <h1 class="text-[16px] md:text-[23px] text-[#2D3B3B] font-[500]">
+                $1,000
+              </h1>
+            </div>
+          </div>
+
+          <!-- Slider -->
+        </div>
         <!-- .... -->
         <div
           class="bg-[#F9F5F4] mt-5 py-8 flex lg:items-center px-2 flex-col lg:flex-row gap-5"
